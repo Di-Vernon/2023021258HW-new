4e47abe9ebf694fd4114c20f1abe955b6d97db08
Di-Vernon <Liquidangel6922@gmail.com>
2023-04-16 13:43:11 +0900

    First Commit

---
+++ pages/posts/first-page.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+export default function Firstpage(){
+    return(
+        <div>
+            <h1>Magic is my hobby</h1>
+            <h2>
+                Magic turns impossible to possible. It makes audiences' imagination true.
+                This is what makes magic brilliant
+            </h2>
+            <h3>
+                I do mentalism. It is an act that plays with people's mind.
+                I read, manipulate audiences' mind, making my act look more impossible.
+                ...Nowadays, I'm planning to open a big concert with my magic team in Hanyang university.``
+            </h3>
+            <Link href="/">Back to home</Link>
+        </div>
+    )
+}
